4e3f029d4e9848ccdf5b13023a34844a75cc2236
Mark Trovinger <mark.trovinger@gmail.com>
2026-01-17 20:42:54 +0000

    using the env checker

--- experiment_replication/ppo_ant.py
+++ experiment_replication/ppo_ant.py
@@ -1,10 +1,9 @@
 import robust_gymnasium as gym
 from robust_gymnasium.configs.robust_setting import get_config
-args = get_config().parse_args()
-args.env_name = "Ant-v4"
-args.noise_factor = "action"
+import gymnasium as gym
 #from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3 import PPO
+from stable_baselines3.common.env_checker import check_env
 from action_injection_wrapper import VectorActionInjection, ActionInjectionWrapper
 
 
@@ -13,8 +12,11 @@ def main():
     robust_args = get_config().parse_args()
     robust_args.env_name = "Ant-v4"
     robust_args.noise_factor = "state"
-    env = gym.make(args.env_name, render_mode="rgb_array")
-    env = ActionInjectionWrapper(env, robust_input={"robust_type" : "state","robust_config": robust_args})
+    #env_name = "Ant-v4"
+    env = gym.make(robust_args.env_name, render_mode="rgb_array")
+    #env = gym.make(env_name, render_mode="rgb_array")
+    check_env(env)
+    #env = ActionInjectionWrapper(env, robust_input={"robust_type" : "state","robust_config": robust_args})
     #env_vec = make_vec_env(args.env_name)
 
     model = PPO("MlpPolicy", env=env, verbose=1)
